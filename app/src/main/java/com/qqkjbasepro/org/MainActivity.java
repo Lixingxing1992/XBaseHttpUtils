6b0934b0dcf7b20a1d88a7ed013c0e9e28d5f737
Lixingxing1992 <497655880@qq.com>
2019-02-27 14:21:24 +0800

    添加README.md

--- app/src/main/java/com/qqkjbasepro/org/MainActivity.java
+++ app/src/main/java/com/qqkjbasepro/org/MainActivity.java
@@ -6,10 +6,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.app.org.base.BaseActivity;
-import com.qqkjbasepro.org.activitylanch.OneActivity;
 import com.qqkjbasepro.org.util.TestHttp;
-import com.qqkjbasepro.org.view.TextViewDialog;
-import com.qqkjbasepro.org.view.TextViewProgress;
 
 public class MainActivity extends BaseActivity {
 
@@ -19,12 +16,6 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        try {
-            getClassLoader().loadClass("Test");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
 
     }
 
@@ -50,17 +41,8 @@ public class MainActivity extends BaseActivity {
 
     public void onBtnClick(View view) {
         switch (view.getId()) {
-            case R.id.progress:
-                startActivity(new Intent(baseContext, TextViewProgress.class));
-                break;
-            case R.id.dialog:
-                startActivity(new Intent(baseContext, TextViewDialog.class));
-                break;
             case R.id.http:
                 startActivity(new Intent(baseContext, TestHttp.class));
-                break;
-            case R.id.activity:
-                startActivity(new Intent(baseContext, OneActivity.class));
                 break;
             default:
                 break;
